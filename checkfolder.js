69004176e788898889549a78041cd6343409a1bb
alievdenis1 <alievdenis1@ya.ru>
2018-08-15 22:10:39 +0500

    Add files via upload

--- checkfolder.js
+++ checkfolder.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
-var mkdirp = require('mkdirp');
 const path = require('path');
 
-function addFolder (dirname) {
-  mkdirp(dirname, err => {
+var _addFolder = (dirname) => {
+  fs.mkdir(dirname, err => {
     if (err) console.error(err);
     else console.log('new directory:' + dirname);
   });
-}
+};
 
-function writeImg (dirname, localBase, nameFile) {
+var _writeImg = (dirname, localBase, nameFile) => {
   fs.readFile(localBase, (err, data) => {
     if (err) throw err;
     fs.writeFile(path.join(dirname, nameFile), data, (err) => {
@@ -17,13 +16,13 @@ function writeImg (dirname, localBase, nameFile) {
       console.log(path.join(dirname, nameFile));
     });
   });
-}
+};
 
 var checkFolder = (dirname, localBase, nameFile) => {
   if (!fs.existsSync(dirname)) {
-    addFolder(dirname);
+    _addFolder(dirname);
   } else {
-    writeImg(dirname, localBase, nameFile);
+    _writeImg(dirname, localBase, nameFile);
   }
   return 0;
 };
--- hw1.js
+++ hw1.js
@@ -1,15 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const addfolder = require('./checkfolder');
+const con = require('./config');
 
-const filename = 'img';
-const base = path.join(__dirname, filename);
-const filename2 = filename + 'Copy';
-const base2 = path.join(__dirname, filename2);
+if (!fs.existsSync(con.base2)) fs.mkdirSync(con.base2);
 
-if (!fs.existsSync(base2)) fs.mkdirSync(base2);
-
-const readDir = (base, level) => {
+const readDir = (base) => {
   const files = fs.readdirSync(base);
 
   files.forEach(item => {
@@ -17,11 +13,11 @@ const readDir = (base, level) => {
     let state = fs.statSync(localBase);
 
     if (state.isDirectory()) {
-      readDir(localBase, level + 1);
+      readDir(localBase);
     } else {
-      addfolder(path.join(base2, item[0].toUpperCase()), localBase, item);
+      addfolder(path.join(con.base2, item[0].toUpperCase()), localBase, item);
     }
   });
 };
 
-readDir(base, 0);
+readDir(con.base, 0);
